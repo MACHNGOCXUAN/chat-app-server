d222eaf7e064c00c2d74392e6970591e328bf27f
unknown <luuly1163@gmail.com>
2025-04-11 20:29:53 +0700

    them avtURl register

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -109,7 +109,7 @@ const checkUserExists = async (req, res) => {
 
 const register = async (req, res) => {
 
-  const { username, phoneNumber, dateOfBirth, gender, password, email, avatarURL } = req.body
+  const { username, phoneNumber, dateOfBirth, gender, password, email, avatarURL  } = req.body
   console.log(req.body);
   
   try {
