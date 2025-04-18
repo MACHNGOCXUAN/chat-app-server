367c6e8977515abbc9a770f96ef0cf9846985dd7
unknown <luuly1163@gmail.com>
2025-04-19 01:51:00 +0700

    LuLy

--- src/sockets/index.js
+++ src/sockets/index.js
@@ -1,19 +1,19 @@
 import authHandler from "./handlers/authHandler.js";
 import userModel from "../models/userModel.js";
 import conversationModel from "../models/conversationModel.js";
-import messageModel from '../models/messageModel.js';
+import messageModel from '../models/messageModel.js'
 import uploadFile from "../utils/file.service.js";
+
 
 const socketServer = (io) => {
   io.on('connection', (socket) => {
     console.log('New client connected:', socket.id);
 
     // Tham gia cuộc trò chuyện
-    socket.on('join_conversation', async ({ senderId, rereceiveId }) => {
+    socket.on('join_conversation', async ({senderId, rereceiveId}) => {
       console.log('join_conversation called with:');
       console.log('senderId:', senderId);
       console.log('rereceiveId:', rereceiveId);
-
       let conversation = await conversationModel.findOne({
         type: 'private',
         members: {
@@ -21,34 +21,37 @@ const socketServer = (io) => {
           $size: 2
         }
       });
-
       if (!conversation) {
         conversation = await conversationModel.create({
           name: `${senderId} - ${rereceiveId}`,
           members: [senderId, rereceiveId]
         });
       }
-
       socket.join(conversation._id.toString());
       socket.emit("joined_room", { conversationId: conversation._id });
     });
 
-    // Tham gia phòng cá nhân (cho realtime)
     socket.on('joinUserRoom', (userId) => {
       socket.join(userId);
       console.log(`User ${userId} joined their room`);
     });
 
+    // socket.on('leave_conversation', (conversationId) => {
+    //   socket.leave(conversationId);
+    //   console.log(`User left conversation: ${conversationId}`);
+    // });
+
+    
     // Gửi tin nhắn
     socket.on("sendMessage", async (data) => {
       try {
         const { conversationId, senderId, content, messageType } = data;
         let messageContent = content;
-
+        
         if (messageType !== 'text') {
           messageContent = await uploadFile(content.file);
         }
-
+    
         const newMessage = new messageModel({
           conversationId,
           senderId,
@@ -56,16 +59,16 @@ const socketServer = (io) => {
           messageType,
           is_last_message: true,
         });
-
+    
         const savedMessage = await newMessage.save();
-
+    
         const updatedConversation = await conversationModel.findByIdAndUpdate(
           conversationId,
           {
             lastMessage: savedMessage._id,
             updatedAt: new Date(),
           },
-          {
+          { 
             new: true,
             populate: [
               { path: 'members', select: 'username avatarURL' },
@@ -73,10 +76,10 @@ const socketServer = (io) => {
             ]
           }
         );
-
+    
         const messageWithSender = await messageModel.findById(savedMessage._id)
           .populate('senderId', 'username avatarURL');
-
+    
         socket.to(conversationId).emit('receive_message', messageWithSender);
         socket.emit('message_sent', messageWithSender);
 
@@ -93,50 +96,21 @@ const socketServer = (io) => {
     socket.on('recall_message', async (data) => {
       try {
         const { messageId, conversationId } = data;
-
         const updatedMessage = await messageModel.findByIdAndUpdate(
           messageId,
-          {
-            content: 'Tin nhắn đã được thu hồi',
-            messageType: 'text',
-            edited: true
-          },
+          { content: 'Tin nhắn đã được thu hồi', messageType: 'text', edited: true },
           { new: true }
         ).populate('senderId', 'username avatarURL');
 
         io.to(conversationId).emit('message_recalled', updatedMessage);
+
       } catch (error) {
         console.error('Error recalling message:', error);
-        socket.emit('recall_error', { error: 'Không thể thu hồi tin nhắn' });
+        socket.emit('recall_error', { error: 'Không thể thu hổi tin nhắn' });
       }
     });
 
-    // ✅ Cập nhật danh sách bạn bè realtime
-    socket.on('update_friends', async ({ userId }) => {
-      try {
-        const user = await userModel.findById(userId);
-        if (!user || !user.friends) return;
-
-        const friendDetails = await Promise.all(
-          user.friends.map(async (friend) => {
-            const friendData = await userModel.findById(friend.friendId);
-            return {
-              id: friendData._id,
-              name: friendData.username,
-              avatar: friendData.avatarURL || '/static/images/avatar/default.jpg',
-              phoneNumber: friendData.phoneNumber,
-              lastMessage: '',
-              time: '',
-              type: 'person'
-            };
-          })
-        );
-
-        io.to(userId).emit('updateFriendsList', friendDetails);
-      } catch (err) {
-        console.error("Lỗi khi cập nhật danh sách bạn bè:", err);
-      }
-    });
+    
 
     // socket.on('typing', (data) => {
     //   const { conversationId, userId } = data;
@@ -148,10 +122,11 @@ const socketServer = (io) => {
     //   socket.to(conversationId).emit('user_stop_typing', { userId });
     // });
 
+
     socket.on('disconnect', () => {
       console.log('Client disconnected:', socket.id);
     });
   });
-};
+}
 
-export default socketServer;
+export default socketServer
